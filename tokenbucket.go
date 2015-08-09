9fecbbdf1ba4b743d81cf4b386aa20d087519573
ryanfowler <ryan.fowler19@gmail.com>
2015-08-08 23:22:29 -0400

    Overhaul token bucket
    Add Close(), Pause(), Resume()
    Add IsClosed(), IsPaused()
    Add NewBurstyTBucket()

--- tokenbucket.go
+++ tokenbucket.go
@@ -29,66 +29,221 @@ import (
 
 // TBucket is a struct representing the token bucket algorithm.
 //
-// For more information on the token bucket algorithm, look at:
+// All provided functions are safe for concurrent use. It is designed to be a
+// fast, lightweight, and lock-free implementation of the token bucket algorithm.
+//
+// For more information on the token bucket algorithm, check out:
 // https://en.wikipedia.org/wiki/Token_bucket
 type TBucket struct {
 	// available tokens
 	tokens int64
-	// burst is the maximum number of tokens that can be in the bucket
+	// bsize is the maximum number of tokens that can fit in the bucket
+	bsize int64
+	// burst is the number of tokens to add to the bucket for each 'tick'
 	burst int64
 	// ticker is the timer that adds tokens to the bucket
 	ticker *time.Ticker
+	// cch is the channel that listens for a close event
+	cch chan struct{}
+	// closed indicates whether the bucket is closed (1) or not (0)
+	closed uint32
+	// prch is the channel that listens for a pause/resume event
+	prch chan struct{}
+	// paused indicates whether the bucket is paused (1) or not (0)
+	paused uint32
 }
 
-// NewTBucket will create a new token bucket queue instance.
+// NewTBucket will create a new token bucket instance.
 //
-// The parameter "burst" is the maximum bucket size and the parameter "dur" is
-// the time interval another token will be added to the bucket.
+// The parameter "bsize" is the maximum bucket size and the parameter "dur" is
+// the time interval another token will be added to the bucket. Using this
+// function is equivilant to calling NewBurstyTBucket(bsize, 1, dur).
 //
-// To learn more about the token bucket algorithm, look at:
+// To learn more about the token bucket algorithm, check out:
 // https://en.wikipedia.org/wiki/Token_bucket
-func NewTBucket(burst int64, dur time.Duration) *TBucket {
-	if burst <= 1 {
+func NewTBucket(bsize int64, dur time.Duration) *TBucket {
+	return NewBurstyTBucket(bsize, 1, dur)
+}
+
+// NewBurstyTBucket will create a new token bucket instance.
+//
+// The parameter "bsize" is the maximum bucket size and the parameter "burst" is
+// the number of tokens to be added to the bucket every time interval "dur".
+//
+// To learn more about the token bucket algorithm, check out:
+// https://en.wikipedia.org/wiki/Token_bucket
+func NewBurstyTBucket(bsize, burst int64, dur time.Duration) *TBucket {
+	if bsize < 1 {
+		bsize = 1
+	}
+	if burst < 1 {
 		burst = 1
 	}
 	tb := &TBucket{
-		tokens: burst,
+		tokens: bsize,
+		bsize:  bsize,
 		burst:  burst,
 		ticker: time.NewTicker(dur),
+		cch:    make(chan struct{}, 1),
+		prch:   make(chan struct{}, 1),
 	}
 	go tb.tick()
 	return tb
 }
 
-// Function that should run in it's own goroutine and adds tokens as necessary
+// This function should run in it's own goroutine and adds tokens to the bucket
+// as necessary, as well as waits for the close and pause/resume commands.
 func (tb *TBucket) tick() {
-	// receive from ticker
 	for {
-		<-tb.ticker.C
-		var done bool
-		for !done {
-			val := atomic.LoadInt64(&tb.tokens)
-			if val < tb.burst {
-				done = atomic.CompareAndSwapInt64(&tb.tokens, val, val+1)
-			} else {
-				done = true
+		select {
+		case <-tb.cch:
+			// close event received, stop timer and return
+			tb.ticker.Stop()
+			return
+		case <-tb.prch:
+			// pause event received, listen for stop or resume events
+			select {
+			case <-tb.cch:
+				// close event received, stop timer and return
+				tb.ticker.Stop()
+				return
+			case <-tb.prch:
+				// resume event received
+			}
+		case <-tb.ticker.C:
+			// timer event, attempt to add token to the bucket
+			var done bool
+			for !done {
+				toks := atomic.LoadInt64(&tb.tokens)
+				if toks < tb.bsize {
+					done = atomic.CompareAndSwapInt64(&tb.tokens, toks, toks+tb.burst)
+				} else {
+					done = true
+				}
 			}
 		}
 	}
 }
 
-// GetTok returns true if a token has been successfully retrieved, and returns
-// false if no token is available.
+// Close stops the internal ticker that adds tokens. The TBucket instance is now
+// permanently closed and cannpt be reopened. When the TBucket will no longer be
+// used, this function must be called to stop the internal timer from continuing
+// to fire.
+//
+// It returns true if the TBucket has been closed, or false if the TBucket has
+// already been closed.
+func (tb *TBucket) Close() bool {
+	if !atomic.CompareAndSwapUint32(&tb.closed, 0, 1) {
+		return false
+	}
+	tb.cch <- struct{}{}
+	return true
+}
+
+// Empty removes all tokens from the bucket. This function does not stop the
+// timer that adds new tokens to the bucket.
+func (tb *TBucket) Empty() {
+	atomic.StoreInt64(&tb.tokens, 0)
+}
+
+// Fill adds the maximum amount of tokens to the bucket (fills the bucket)
+// according to the defined bucket size.
+func (tb *TBucket) Fill() {
+	atomic.StoreInt64(&tb.tokens, tb.bsize)
+}
+
+// FillTo adds "n" tokens to the bucket. The value "n" may be larger than the
+// defined bucket size.
+func (tb *TBucket) FillTo(n int64) {
+	atomic.StoreInt64(&tb.tokens, n)
+}
+
+// GetTok attempts to retrieve a single token from the bucket.
+//
+// It returns true if a token has been successfully retrieved, or returns
+// false if no token is available (the bucket is empty).
 func (tb *TBucket) GetTok() bool {
 	var done bool
-	var val int64
 	for !done {
-		val = atomic.LoadInt64(&tb.tokens)
-		if val > 0 {
-			done = atomic.CompareAndSwapInt64(&tb.tokens, val, val-1)
+		toks := atomic.LoadInt64(&tb.tokens)
+		if toks > 0 {
+			done = atomic.CompareAndSwapInt64(&tb.tokens, toks, toks-1)
 		} else {
 			return false
 		}
 	}
+	return true
+}
+
+// GetToks attempts to retrieve "n" tokens from the bucket.
+//
+// It returns true if "n" tokens have been successfully retrieved, or returns
+// false if there are not enough tokens available.
+//
+// The provided parameter "n" cannot be smaller than 1. If a smaller value is
+// provided, the value 1 will be used.
+func (tb *TBucket) GetToks(n int64) bool {
+	if n < 1 {
+		n = 1
+	}
+	var done bool
+	for !done {
+		toks := atomic.LoadInt64(&tb.tokens)
+		if toks >= n {
+			done = atomic.CompareAndSwapInt64(&tb.tokens, toks, toks-n)
+		} else {
+			return false
+		}
+	}
+	return true
+}
+
+// IsClosed returns true if the TBucket has been closed. It returns false if
+// it is still open.
+func (tb *TBucket) IsClosed() bool {
+	if atomic.LoadUint32(&tb.closed) == 0 {
+		return false
+	}
+	return true
+}
+
+// IsPaused returns true if the TBucket has been paused. It returns false if
+// it is not in a paused state.
+func (tb *TBucket) IsPaused() bool {
+	if atomic.LoadUint32(&tb.paused) == 0 {
+		return false
+	}
+	return true
+}
+
+// Pause temporarily pauses the TBucket from adding new tokens to the bucket.
+// When paused, tokens in the bucket can still be retrieved with GetTok or
+// GetToks. The TBucket can be closed (with Close) or can be resumed (with
+// Resume).
+//
+// This function should be used when the TBucket should only be temporarily
+// paused. If the TBucket will not be used again, Close should be called to
+// stop the internal timer.
+//
+// Pause returns true if the TBucket has been paused, or false if the TBucket
+// is already in a paused state.
+func (tb *TBucket) Pause() bool {
+	if tb.IsClosed() || !atomic.CompareAndSwapUint32(&tb.paused, 0, 1) {
+		return false
+	}
+	tb.prch <- struct{}{}
+	return true
+}
+
+// Resume resumes a TBucket in a paused state and starts adding new tokens to
+// the bucket again.
+//
+// Resume returns true if the TBucket has been resumed, or false if the TBucket
+// is not in a paused state.
+func (tb *TBucket) Resume() bool {
+	if tb.IsClosed() || !atomic.CompareAndSwapUint32(&tb.paused, 1, 0) {
+		return false
+	}
+	tb.prch <- struct{}{}
 	return true
 }
--- tokenbucket_test.go
+++ tokenbucket_test.go
@@ -23,15 +23,102 @@
 package ratelim
 
 import (
-	//"fmt"
 	"testing"
 	"time"
 )
 
 func TestCreateTBucket(t *testing.T) {
 	tb := NewTBucket(-10, time.Second)
-	if tb.burst != 1 {
+	if tb.bsize != 1 {
+		t.Error("Bucket size value should be 1 when a lower value provided")
+	}
+	tb2 := NewBurstyTBucket(-10, -15, time.Second)
+	if tb2.burst != 1 {
 		t.Error("Burst value should be 1 when a lower value provided")
+	}
+}
+
+func TestTBucketPause(t *testing.T) {
+	tb := NewTBucket(10, time.Millisecond)
+	if tb.Resume() {
+		t.Error("Resume on non-paused TBucket returned true")
+	}
+	if tb.IsPaused() {
+		t.Error("IsPaused returned true on non-paused TBucket")
+	}
+	if !tb.Pause() {
+		t.Error("Pause TBucket failed")
+	}
+	if !tb.IsPaused() {
+		t.Error("IsPaused returned false on paused TBucket")
+	}
+	tb.Empty()
+	if tb.tokens != 0 {
+		t.Error("Bucket not emptied")
+	}
+	time.Sleep(time.Millisecond * 10)
+	if tb.tokens != 0 {
+		t.Error("Tokens added to paused TBucket")
+	}
+	if tb.Pause() {
+		t.Error("Pause called on paused TBucket returned true")
+	}
+	if !tb.Resume() {
+		t.Error("Resume TBucket failed")
+	}
+	time.Sleep(time.Millisecond * 10)
+	if tb.tokens == 0 {
+		t.Error("Tokens not added to bucket after Resume")
+	}
+	tb.Pause()
+	toks := tb.tokens
+	time.Sleep(time.Millisecond * 5)
+	if !tb.Close() {
+		t.Error("Close in paused TBucket returned false")
+	}
+	time.Sleep(time.Millisecond * 5)
+	if tb.tokens != toks {
+		t.Error("Tokens added after TBucket closed when paused")
+	}
+}
+
+func TestTBucketClose(t *testing.T) {
+	tb := NewTBucket(10, time.Millisecond)
+	if tb.IsClosed() {
+		t.Error("IsClosed returned true on open TBucket")
+	}
+	if !tb.Close() {
+		t.Error("Close returned false on open TBucket")
+	}
+	if !tb.IsClosed() {
+		t.Error("IsClosed returned false on closed TBucket")
+	}
+	tb.Empty()
+	time.Sleep(time.Millisecond * 10)
+	if tb.tokens > 0 {
+		t.Error("Tokens added to closed TBucket")
+	}
+	if tb.Close() {
+		t.Error("Close returned true on closed TBucket")
+	}
+}
+
+func TestTBucketFill(t *testing.T) {
+	tb := NewTBucket(10, time.Second)
+	if !tb.Pause() {
+		t.Error("Pause returned false on non-paused TBucket")
+	}
+	tb.Empty()
+	if tb.tokens != 0 {
+		t.Error("Tokens remain in bucket after calling Empty")
+	}
+	tb.Fill()
+	if tb.tokens != 10 {
+		t.Error("Bucket not full after calling Fill:", tb.tokens)
+	}
+	tb.FillTo(5)
+	if tb.tokens != 5 {
+		t.Error("Incorrect number of tokens in bucket after calling FillTo")
 	}
 }
 
@@ -49,16 +136,63 @@ func TestTBucketGetTok(t *testing.T) {
 	}
 }
 
-func BenchmarkGet(b *testing.B) {
-	tb := NewTBucket(10, time.Millisecond)
+func TestTBucketGetToks(t *testing.T) {
+	tb := NewTBucket(10, time.Second)
+	tb.Pause()
+	if !tb.GetToks(-10) {
+		t.Error("GetToks returned false when enough tokens exist in bucket")
+	}
+	if tb.tokens != 9 {
+		t.Error("Incorrect number of tokens removed from bucket")
+	}
+	var oks int
+	for i := 0; i < 20; i++ {
+		ok := tb.GetToks(1)
+		if ok {
+			oks += 1
+		}
+	}
+	if oks != 9 {
+		t.Error("Incorrect number of tokens provided with GetToks")
+	}
+}
+
+func BenchmarkGetFail(b *testing.B) {
+	tb := NewTBucket(1, time.Millisecond)
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		tb.GetTok()
 	}
 }
 
-func BenchmarkGet2(b *testing.B) {
-	tb := NewTBucket(10, time.Millisecond)
+func BenchmarkGetSuc(b *testing.B) {
+	tb := NewTBucket(10000000000, time.Millisecond)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		tb.GetTok()
+	}
+}
+
+func BenchmarkGetPFail(b *testing.B) {
+	tb := NewTBucket(1, time.Millisecond)
+	go func() {
+		for i := 0; i < 100000000; i++ {
+			tb.GetTok()
+		}
+	}()
+	go func() {
+		for i := 0; i < 100000000; i++ {
+			tb.GetTok()
+		}
+	}()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		tb.GetTok()
+	}
+}
+
+func BenchmarkGetPSuc(b *testing.B) {
+	tb := NewTBucket(100000000, time.Millisecond)
 	go func() {
 		for i := 0; i < 100000000; i++ {
 			tb.GetTok()
