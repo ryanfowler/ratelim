ab87d3002472f80f90731d1545451024ee1745bc
ryanfowler <ryan.fowler19@gmail.com>
2015-08-09 23:59:47 -0400

    Update Counter type

--- counter.go
+++ counter.go
@@ -27,34 +27,33 @@ import (
 )
 
 type Counter struct {
-	cnt *int64
+	cnt int64
 }
 
 func NewCounter() *Counter {
-	var cnt int64
-	return &Counter{cnt: &cnt}
+	return &Counter{}
 }
 
 func (c *Counter) Inc() int64 {
-	return atomic.AddInt64(c.cnt, 1)
+	return atomic.AddInt64(&c.cnt, 1)
 }
 
 func (c *Counter) IncBy(i int64) int64 {
-	return atomic.AddInt64(c.cnt, i)
+	return atomic.AddInt64(&c.cnt, i)
 }
 
 func (c *Counter) Dec() int64 {
-	return atomic.AddInt64(c.cnt, -1)
+	return atomic.AddInt64(&c.cnt, -1)
 }
 
 func (c *Counter) DecBy(i int64) int64 {
-	return atomic.AddInt64(c.cnt, -i)
+	return atomic.AddInt64(&c.cnt, -i)
 }
 
 func (c *Counter) Zero() int64 {
-	return atomic.SwapInt64(c.cnt, 0)
+	return atomic.SwapInt64(&c.cnt, 0)
 }
 
 func (c *Counter) SetTo(i int64) int64 {
-	return atomic.SwapInt64(c.cnt, i)
+	return atomic.SwapInt64(&c.cnt, i)
 }
